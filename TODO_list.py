7183731af9ed9b4d27e2b0fa4c84a3f14b2e4420
sziarece <kanyadi.richard@gmail.com>
2024-01-30 09:17:40 +0100

    Delete task function.py

--- TODO_list.py
+++ TODO_list.py
@@ -6,6 +6,13 @@ class TodoList:
         self.tasks.append({"task": task, "completed": False})
         print(f'Task "{task}" added successfully!')
 
+    def delete_task(self, task_index):
+        if 0 <= task_index < len(self.tasks):
+            deleted_task = self.tasks.pop(task_index)
+            print(f'Task: "{deleted_task["task"]}" deleted successfully!')
+        else:
+            print('The given task index  is invalid. Please try again.')
+
     def show_tasks(self):
         if not self.tasks:
             print('The task list is empty.')
@@ -19,12 +26,16 @@ def main():
     todo_list = TodoList()
 
     while True:
-        print('\n1. Add Task\n4. Show Tasks\n5. Exit')
+        print('\n1. Add Task\n2. Delete Task\n4. Show Tasks\n5. Exit')
         choice = input('Enter your choice of command (1-5): ')
 
         if choice == '1':
             task = input('Enter the task: ')
             todo_list.add_task(task)
+        elif choice == '2':
+            todo_list.show_tasks()
+            index = int(input('Enter the task index to delete: '))
+            todo_list.delete_task(index - 1)
         elif choice == '4':
             todo_list.show_tasks()
         elif choice == '5':

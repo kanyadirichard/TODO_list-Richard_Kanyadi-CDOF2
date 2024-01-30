526f2afdb7e13b15d66bb033520846f7ba7caad9
sziarece <kanyadi.richard@gmail.com>
2024-01-30 09:03:04 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='{TODO_list}',
+ version='1.0',
+ author='{Richárd Kányádi}',
+ license='MIT',
+ long_description=open('README.md').read(),
+)

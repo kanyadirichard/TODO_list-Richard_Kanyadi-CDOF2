bfc346fc1447b1c5ca729eedca9d44a253acd04d
sziarece <kanyadi.richard@gmail.com>
2024-01-30 09:21:00 +0100

    Complete task function.py

--- TODO_list.py
+++ TODO_list.py
@@ -22,11 +22,18 @@ class TodoList:
                 status = "Completed" if task["completed"] else "Pending"
                 print(f'{index + 1}. {task["task"]} - {status}')
 
+    def complete_task(self, task_index):
+        if 0 <= task_index < len(self.tasks):
+            self.tasks[task_index]["completed"] = True
+            print(f'Task: "{self.tasks[task_index]["task"]}" marked as completed!')
+        else:
+            print('Invalid task index. Please try again.')
+
 def main():
     todo_list = TodoList()
 
     while True:
-        print('\n1. Add Task\n2. Delete Task\n4. Show Tasks\n5. Exit')
+        print('\n1. Add Task\n2. Delete Task\n3. Complete Task\n4. Show Tasks\n5. Exit')
         choice = input('Enter your choice of command (1-5): ')
 
         if choice == '1':
@@ -36,6 +43,10 @@ def main():
             todo_list.show_tasks()
             index = int(input('Enter the task index to delete: '))
             todo_list.delete_task(index - 1)
+        elif choice == '3':
+            todo_list.show_tasks()
+            index = int(input('Enter the task index to mark as complete: '))
+            todo_list.complete_task(index - 1)
         elif choice == '4':
             todo_list.show_tasks()
         elif choice == '5':
